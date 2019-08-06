167e0723ba0e37050af9ec4036c9ac29d8412207
namnh-0652 <nguyen.huu.nam@sun-asterisk.com>
2019-08-06 17:09:44 +0700

    [Improvement] Repackage uizalivestream module

---
+++ uizabroadcast/src/main/java/io/uiza/broadcast/util/UzLiveVideoMode.java
@@ -0,0 +1,10 @@
+package io.uiza.broadcast.util;
+
+public enum UzLiveVideoMode {
+    /** Live video Full HD. */
+    MODE_FULL_HD,
+    /** Live video HD. */
+    MODE_HD,
+    /** Live video SD. */
+    MODE_SD
+}
